0b5babbc08a5cb61dc0732613bab01fc4f8c70ca
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-25 12:24:43 +0300

    создана форма под авторизацию и регистрацию

--- product_rating_app/routes.py
+++ product_rating_app/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from product_rating_app.forms import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 
 @app.route("/")
@@ -8,6 +10,7 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/new")
-# def index():
-#     return render_template()
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
